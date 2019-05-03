2d6f27195ebf40edeb6b62745da7530b7ef81090
Jonathan Chancey <jchancey@ucmerced.edu>
2019-05-02 21:53:38 -0700

    tested and updated placer

--- kinetic_file_placer.sh
+++ kinetic_file_placer.sh
@@ -1,10 +1,17 @@
+read -n 1 -s -r -p "Please move all files to your ~/Downloads folder. Press any key to continue..."
+
+
+source /opt/ros/kinetic/setup.bash
+
 roscd husky_gazebo/worlds/
-sudo mv /resources/final2019.world .
+sudo mv ~/Downloads/final2019.world .
 roscd husky_gazebo/launch/
-sudo mv /resources/huskyfinal.launch .
-sudo mv /resources/playpenfinal.launch .
+sudo mv ~/Downloads/huskyfinal.launch .
+sudo mv ~/Downloads/playpenfinal.launch .
 roscd husky_navigation/maps/
-sudo mv /resources/mapfinal.yaml .
-sudo mv /resources/mapfinal.pgm .
+sudo mv ~/Downloads/mapfinal.yaml .
+sudo mv ~/Downloads/mapfinal.pgm .
 roscd husky_navigation/launch/
-sudo mv /resources/amcl_final.launch .
+sudo mv ~/Downloads/amcl_final.launch .
+
+read -n 1 -s -r -p "To launch the simulation run roslaunch husky_gazebo huskyfinal.launch"
